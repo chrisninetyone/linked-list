67e5dc51192a959bad0dfe28c1f4d4e25d026916
Chris <chrisninetyone@gmail.com>
2018-07-31 15:26:29 -0600

    Basic jQuery setup

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
 
 
 
-//defining the button object for enter button
-const enterButton = document.getElementById("enter-guess");
 
 //defining the website title input as a string to display
-const websiteTitle = document.getElementById("website-title-input");
+var websiteTitle = $("#website-title-input").val();
 
 //defining the url input as a string to display (link?)
-const websiteUrl = document.getElementById('website-url-input');
+var websiteUrl = $('#website-url-input').val();
 
-//declaring variables of bookmark section 1
-const website1 = document.getElementById('website-1');
-const url1 = document.getElementById("url-1");
+// //Make a function to create a bookmark
+// createBookmark () => {
 
-const bookmark = document.getElementById("bookmarks");
+// }
 
-markAsRead = () => {
 
-}
-//create function which creates a bookmark section using innerHTML and .createElement .createTextNode .appendChild()
-createBookmark = () => {
-    console.log('boom')
-   $('#website-1').text('boom')
-}
-
-//add click event to add url to bookmarks div
 $('.enter-guess').on('click', () => {
-    $('.website-1').text('boom')
+    var websiteTitle = $("#website-title-input").val();
+    var websiteUrl = $('#website-url-input').val();
+    $('.website-title').text(websiteTitle);
+    $('.website-url').text(websiteUrl);
+
 })
