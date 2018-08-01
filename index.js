8a26b44f2fd86d1a6b87a793d6d272c4eae2bbe6
Noah Peden <noahpeden@gmail.com>
2018-08-01 15:42:54 -0600

    A little help with parent node stuff.

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-
-
-    createBookmark = () =>{
-        var readBtn = $(".read-btn");
-        var websiteTitle = $("#website-title-input").val();
-        var websiteUrl = $('#website-url-input').val();
-        $('.bookmarks').append(`<div>${websiteTitle}${websiteUrl}<button id="read-btn">Read</button><button id="delete-btn">Delete</button></div>`);
-    };
+createBookmark = () => {
+	var readBtn = $('.read-btn');
+	var websiteTitle = $('#website-title-input').val();
+	var websiteUrl = $('#website-url-input').val();
+	$('.bookmarks').append(
+        `<div>
+            <h3>${websiteTitle}${websiteUrl}</h3>
+            <button class="read-btn">Read</button>
+            <button class="delete-btn">Delete</button>
+        </div>`
+	);
+};
 
 removeBookmark = () => {
-    $(this).parent().remove();
+	console.log('boom');
+	$(this)
+		.parent()
+		.remove();
 };
 
 $('.enter-guess').on('click', () => {
-    createBookmark();
-
+	createBookmark();
 });
 
 $('#delete-btn').on('click', () => {
-    removeBookmark();
+	removeBookmark();
+});
+
+// you have to select the parent of the HTML element that is being selected for jQuery to recognize it
+
+$('.bookmarks').on('click', '.delete-btn', () => {
+    console.log('boom')
+	$(this)
+		.parent('div')
+		.remove();
 });
