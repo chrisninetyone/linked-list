59c1552103201058aca8a399e08d75190c247ea9
Chris <chrisninetyone@gmail.com>
2018-08-08 18:26:18 -0600

    alerts

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-createBookmark = () => {
+createBookmark = function() {
     'use strict';
 	const websiteTitle = $('#website-title-input').val();
     const websiteUrl = $('#website-url-input').val();
@@ -7,16 +7,13 @@ createBookmark = () => {
 
 	$('.bookmarks').append(
         `<div class="outer-bookmark-box">
-
-                <h3 class="website-title">${websiteTitle}</h3>
-                <h4 class="website-url">${websiteUrl}</h4>
-                    <button class="read-btn">Read</button>
-                    <button class="delete-btn">Delete</button>
-
+            <h3 class="website-title">${websiteTitle}</h3>
+            <h4 class="website-url">${websiteUrl}</h4>
+                <button class="read-btn">Read</button>
+                <button class="delete-btn">Delete</button>
         </div>`
     );
     $('.bookmarks').on('click', '.delete-btn', function() {
-        console.log('boom');
         $(this).parent().remove();
     });
 
@@ -24,15 +21,27 @@ createBookmark = () => {
         if ($(this).parent().hasClass('read')) {
             $(this).parent().removeClass('read');
         } else {
-            $(this).parent().addClass('read')
+            $(this).parent().addClass('read');
         }
     });
 };
 
+// function disable() {
+//     if ($('.website-title').val() ==== "" || ($('.website-url').val() === "")) {
+//         $('.enter-guess').prop('disabled', true);
+//     }
+// }
 
-$('.enter-guess').on('click', () => {
-	createBookmark();
+$('.enter-guess').on('click', function() {
+        createBookmark();
 });
+
+//Add a counter
+
+// $(".enter-guess").click(function() {
+//     count++;
+//     $("#counter").html("My current count is: "+count);
+// });
 
 
 // you have to select the parent of the HTML element that is being selected for jQuery to recognize it
